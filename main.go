9444b99dcbc76ca00ccc1a85e0575f499f3fc7c4
ohwoo-kwon <kwonow1@gmail.com>
2022-02-28 22:52:54 +0900

    1.6 If with a Twist

--- main.go
+++ main.go
@@ -4,16 +4,13 @@ import (
 	"fmt"
 )
 
-func superAdd(numbers ...int) int {
-	result := 0
-	for idx, number := range numbers{
-		fmt.Println(idx, number)
-		result += number
+func canIDrink(age int) bool {
+	if koreanAge := age + 2; koreanAge < 18 {
+		return false
 	}
-	return result
+	return true
 }
 
 func main() {
-	total := superAdd(4, 3, 1, 2, 5, 6, 7, 8, 9, 10)
-	fmt.Println(total)
+	fmt.Println(canIDrink(16))
 }
