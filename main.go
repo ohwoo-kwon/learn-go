a437b7933b1cfbfbb8a68fe42b6a3d034a1be804
ohwoo-kwon <kwonow1@gmail.com>
2022-03-01 10:48:31 +0900

    1.9 Arrays and Slices

--- main.go
+++ main.go
@@ -3,9 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	a := 2
-	b := &a
-	*b = 20
-	fmt.Println(a, *b)
+	ohmygirl := [7] string{"hyojung", "mimi", "yooa"}
+	ohmygirl[3] = "seunghee"
+	ohmygirl[4] = "jiho"
+	ohmygirl[5] = "yubin"
+	ohmygirl[6] = "arin"
+	fmt.Println(ohmygirl)
 
+	names := []string{"ohwoo"}
+	names = append(names, "anonymous1")
+	names = append(names, "anonymous1")
+	fmt.Println(names)
 }
