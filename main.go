73f4e2617f0c8a7672b7319861fc2861bbea6e10
ohwoo-kwon <kwonow1@gmail.com>
2022-03-01 21:09:25 +0900

    3.0 hit URL

--- main.go
+++ main.go
@@ -1,20 +1,35 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-
-	"github.com/ohwoo-kwon/learn-go/mydict"
+	"net/http"
 )
 
+var errRequestFail = errors.New("request failed")
+
 func main() {
-	dictionary := mydict.Dictionary{"hyojung":"leader"}
-	baseWord := "mimi"
-	dictionary.Add(baseWord, "rapper")
-	dictionary.Search(baseWord)
-	dictionary.Delete(baseWord)
-	definition, err := dictionary.Search(baseWord)
-	if err != nil {
-		fmt.Println(err)
+	urls := []string {
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
 	}
-	fmt.Println(definition)
+	for _, url := range urls {
+		hitURL(url)
+	}
+}
+
+func hitURL(url string) error {
+	fmt.Println("Checking: ", url)
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		return errRequestFail
+	}
+	return nil
 }
