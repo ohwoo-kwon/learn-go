c3f8a1c520d68a4072687cdf8d007324022057fb
ohwoo-kwon <kwonow1@gmail.com>
2022-03-02 16:36:11 +0900

    4.2 extractJob part One

--- main.go
+++ main.go
@@ -9,6 +9,14 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+type extractedJob struct {
+	id string
+	title string
+	location string
+	salary string
+	summary string
+}
+
 var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
 var LastpageURL string = "https://kr.indeed.com/jobs?q=python&limit=50&start=9999"
 
@@ -22,7 +30,29 @@ func main() {
 
 func getPage(page int) {
 	pageURL := baseURL + "&start=" + strconv.Itoa(page*50)
-	fmt.Println(pageURL)
+	res, err := http.Get(pageURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+	
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	searchCards := doc.Find(".tapItem")
+
+	searchCards.Each(func(i int, card *goquery.Selection) {
+		id, _ := card.Attr("data-jk")
+		title := card.Find("h2 > span").Text()
+		location := card.Find(".companyLocation").Text()
+		salary := card.Find(".salary-snippet > span").Text()
+		summary :=  card.Find(".job-snippet").Text()
+		fmt.Println(id)
+		fmt.Println(title)
+		fmt.Println(location)
+		fmt.Println(salary)
+		fmt.Println(summary)
+	})
 }
 
 func getPages() int {
