5ca126b4d4c2ec0ca36c4b758f01cbbb7180501c
ohwoo-kwon <kwonow1@gmail.com>
2022-03-02 11:37:09 +0900

    3.7 Fast URLChecker

--- main.go
+++ main.go
@@ -1,22 +1,50 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"time"
+	"net/http"
 )
 
+type requestResult struct {
+	url    string
+	status string
+}
+
+var errRequestFailed = errors.New("request failed")
+
 func main() {
-	c := make(chan string)
-	people := [7]string{"hyojung", "mimi", "yooa", "seunghee", "jiho", "yubin", "arin"}
-	for _, person := range people {
-		go isOMG(person, c)
+	results := make(map[string]string)
+	c := make(chan requestResult)
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
 	}
-	for i:=0;i<len(people);i++{
-		fmt.Println(<-c)
+	for _, url := range urls {
+		go hitURL(url, c)
+	}
+
+	for i:=0;i<len(urls);i++ {
+		result := <- c
+		results[result.url] = result.status
+	}
+	for url, status := range results {
+		fmt.Println(url, status)
 	}
 }
 
-func isOMG(person string, c chan string) {
-	time.Sleep(time.Second * 3)
-	c <- "Hello, " + person
+func hitURL(url string, c chan<- requestResult) {
+	resp, err := http.Get(url)
+	status := "OK"
+	if err != nil || resp.StatusCode >= 400 {
+		status = "FAILED"
+	}
+	c <- requestResult{url: url, status: status}
 }
